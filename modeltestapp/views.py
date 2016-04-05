64e4ed518907719081358f818b5f0241555db9c9
Henry FitzGerald <hfitzgerald@seabreeze.ie>
2016-04-05 12:25:27 +0100

    Added Views and Templates

--- modeltestapp/views.py
+++ modeltestapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from modeltestapp.models import Contact
 
 # Create your views here.
+def get_contacts(request):
+    return render(request, "MT/home.html",
+                  {'contact list': Contact.objects.all(),'foo': "Bar"})
